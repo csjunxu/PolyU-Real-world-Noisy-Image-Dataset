cdf709114d799206e292e3205dfd8410cb6a04ed
XuJun <nankaimathxujun@gmail.com>
2016-12-06 16:44:53 +0800

    add

---
+++ read_dng.m
@@ -0,0 +1,10 @@
+warning off MATLAB:tifflib:TIFFReadDirectory:libraryWarning
+%% poke at a sample DNG file using imfinfo.
+info = imfinfo('DSC01351.dng');
+info.SubIFDs{1};
+%% http://www.mathworks.com/help/matlab/ref/tiff-class.html
+t = Tiff('DSC01351.dng','r');
+offsets = getTag(t,'SubIFD');
+setSubDirectory(t,offsets(1));
+cfa = read(t);
+close(t);
