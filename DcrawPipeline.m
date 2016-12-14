f5e354e7295842ec8e01d736f36bbdc0b3c4bef4
XuJun <nankaimathxujun@gmail.com>
2016-12-14 21:30:50 +0800

    ADD

--- DcrawPipeline.m
+++ DcrawPipeline.m
@@ -9,7 +9,7 @@ lin_bayer = max(0,min(lin_bayer,1)); % 确保没有大于1或小于0的数据;
 imshow(lin_bayer);
 
 %% 2 White Balancing
-wb_multipliers = [1.902344, 1, 1.808594]; % for particular condition, from dcraw;
+wb_multipliers = [1.902345, 1, 1.808593]; % for particular condition, from dcraw;
 mask = wbmask(size(lin_bayer,1),size(lin_bayer,2),wb_multipliers,'rggb');
 balanced_bayer = lin_bayer .* mask;
 imshow(balanced_bayer);
