20bbb82d994e0859767934f2a8f460b736181374
XuJun <nankaimathxujun@gmail.com>
2016-12-16 10:20:10 +0800

    ADD

--- meanRawImages.m
+++ meanRawImages.m
@@ -1,4 +1,5 @@
 clear;
+%% mean of raw images
 Original_image_dir = '20161214/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
@@ -14,5 +15,30 @@ for i = 1:im_num
     meanRaw = meanRaw + raw;    
 end
 meanRaw = meanRaw./im_num;
-imwrite(meanRaw,'mean_ARW_DNG_TIF.tiff');
+imwrite(meanRaw,'mean_Scene1_ARW_DNG_TIF.tiff');
+
+%% mean of dark frames
+Original_image_dir = '20161213/';
+fpath = fullfile(Original_image_dir, '*.tiff');
+im_dir  = dir(fpath);
+im_num = length(im_dir);
+raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
+meanDF = zeros(size(raw));
+for i = 1:im_num
+    %% 0 read the tiff image
+    raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
+    S = regexp(im_dir(i).name, '\.', 'split');
+    rawname = S{1};
+    fprintf('Processing %s. \n', rawname);
+    meanDF = meanDF + raw;    
+end
+meanDF = meanDF./im_num;
+imwrite(meanRaw,'mean_DF_ARW_DNG_TIF.tiff');
+
+%% mean Scene minus mean DF
+meanGT = meanRaw - meanDF;
+imwrite(meanGT,'mean_GT_ARW_DNG_TIF.tiff');
+
+
+
 
