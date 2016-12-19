f35b0b47211130c06758fcef1f68f6608a7f2b78
XuJun <nankaimathxujun@gmail.com>
2016-12-19 16:25:38 +0800

    add

--- meanRawImages.m
+++ meanRawImages.m
@@ -7,7 +7,7 @@ im_num = length(im_dir);
 raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
 meanRaw = zeros(size(raw)); 
 for i = 1:im_num
-    %% 0 read the tiff image
+    %% read the tiff image
     raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
@@ -19,7 +19,7 @@ meanRaw = uint16(meanRaw);
 imshow(meanRaw);
 imwrite(meanRaw,'mean_RAW_ARW2TIF.tiff');
 clear raw meanRaw;
-%% 1 mean of dark frames
+%% mean of dark frames
 Original_image_dir = '20161213/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
@@ -27,7 +27,7 @@ im_num = length(im_dir);
 rawDF = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
 meanDF = zeros(size(rawDF)); 
 for i = 1:im_num
-    %% 0 read the tiff image
+    %% read the tiff image
     rawDF = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
