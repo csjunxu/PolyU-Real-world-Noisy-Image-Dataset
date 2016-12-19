b715b730c45b867a99a35e1440c2b30313358fcf
XuJun <nankaimathxujun@gmail.com>
2016-12-19 16:24:17 +0800

    add

--- meanRawImages.m
+++ meanRawImages.m
@@ -4,38 +4,41 @@ Original_image_dir = '20161219/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-raw = imread(fullfile(Original_image_dir, im_dir(1).name));
-meanRaw = zeros(size(raw), 'uint16'); 
+raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
+meanRaw = zeros(size(raw)); 
 for i = 1:im_num
     %% 0 read the tiff image
-    raw = imread(fullfile(Original_image_dir, im_dir(i).name));
+    raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
-    meanRaw = meanRaw + raw ;
+    meanRaw = meanRaw + raw;
 end
 meanRaw = round(meanRaw./im_num);
+meanRaw = uint16(meanRaw);
 imshow(meanRaw);
 imwrite(meanRaw,'mean_RAW_ARW2TIF.tiff');
-clear raw;
+clear raw meanRaw;
 %% 1 mean of dark frames
 Original_image_dir = '20161213/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-rawdf = imread(fullfile(Original_image_dir, im_dir(1).name));
-meanDF = zeros(size(rawdf), 'uint16'); 
+rawDF = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
+meanDF = zeros(size(rawDF)); 
 for i = 1:im_num
     %% 0 read the tiff image
-    raw = imread(fullfile(Original_image_dir, im_dir(i).name));
+    rawDF = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
-    meanDF = meanDF + raw;
+    meanDF = meanDF + rawDF;
 end
 meanDF = round(meanDF./im_num);
-imwrite(meanRaw,'mean_DF_ARW_DNG_TIF.tiff');
-clear rawdf;
+meanDF = uint16(meanDF);
+imshow(meanDF);
+imwrite(meanDF,'mean_DF_ARW_DNG_TIF.tiff');
+clear rawDF meanDF;
 %% mean Scene minus mean DF
 meanGT = meanRaw - meanDF;
 imwrite(meanGT,'mean_GT_ARW_DNG_TIF.tiff');
