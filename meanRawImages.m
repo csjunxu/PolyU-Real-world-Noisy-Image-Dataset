6ceea57cef85008bc48182860b23809798204d2c
XuJun <nankaimathxujun@gmail.com>
2016-12-19 15:47:47 +0800

    add

--- meanRawImages.m
+++ meanRawImages.m
@@ -5,7 +5,7 @@ fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
 raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
-meanRaw = zeros(size(raw));
+meanRaw = zeros(size(raw), 'uint16'); 
 for i = 1:im_num
     %% 0 read the tiff image
     raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
