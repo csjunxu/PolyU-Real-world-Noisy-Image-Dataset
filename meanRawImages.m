862fc83231736bfd9913d370398ec32777745835
XuJun <nankaimathxujun@gmail.com>
2016-12-19 15:50:25 +0800

    add

--- meanRawImages.m
+++ meanRawImages.m
@@ -4,11 +4,11 @@ Original_image_dir = '20161219/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
+raw = imread(fullfile(Original_image_dir, im_dir(1).name));
 meanRaw = zeros(size(raw), 'uint16'); 
 for i = 1:im_num
     %% 0 read the tiff image
-    raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
+    raw = imread(fullfile(Original_image_dir, im_dir(i).name));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
