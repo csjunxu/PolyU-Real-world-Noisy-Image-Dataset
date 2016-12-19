fbd264271b7fec66de7d9e037374d5b8090b2d19
XuJun <nankaimathxujun@gmail.com>
2016-12-19 15:23:06 +0800

    add

--- meanRawImages.m
+++ meanRawImages.m
@@ -4,15 +4,15 @@ Original_image_dir = '20161219/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-raw = cell(im_num,1);
-meanRaw = zeros(size(double(imread(fullfile(Original_image_dir, im_dir(1).name)))));
+raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
+meanRaw = zeros(size(raw));
 for i = 1:im_num
     %% 0 read the tiff image
-    raw{i,1} = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
+    raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
-    meanRaw = meanRaw + raw{i,1} ;
+    meanRaw = meanRaw + raw ;
 end
 meanRaw = round(meanRaw./im_num);
 imshow(meanRaw);
