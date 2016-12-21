51eee8448c5a5d9269c2aa94cc1a27d649fb470c
XuJun <nankaimathxujun@gmail.com>
2016-12-21 11:19:55 +0800

    a

--- meanRaw_RawDF.m
+++ meanRaw_RawDF.m
@@ -1,7 +1,7 @@
 %% mean Scene minus mean DF
 % 128 is the reset value for Sony A7 II 
 reset = 128;
-meanRaw = imread('mean_RAW_ARW2TIF.tiff');
-meanDF = imread('mean_DF_ARW2TIF.tiff');
+meanRaw = imread('meanRAW_ARW2TIF.tiff');
+meanDF = imread('meanDF_ARW2TIF.tiff');
 meanGT = meanRaw - meanDF + reset; 
-imwrite(meanGT,'mean_GT_ARW2TIF.tiff');
+imwrite(meanGT,'meanGT_ARW2TIF.tiff');
