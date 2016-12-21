2f2d0d4ace4f4404a0bde2a9be114afe52afdf69
XuJun <nankaimathxujun@gmail.com>
2016-12-21 10:14:28 +0800

    add

--- meansRGBImages.m
+++ meansRGBImages.m
@@ -7,7 +7,7 @@ im_num = length(im_dir);
 sRGB = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
 meansRGB = zeros(size(sRGB)); 
 for i = 1:im_num
-    %% read the tiff image
+    %% read the tiff image 
     sRGB = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
@@ -16,7 +16,7 @@ for i = 1:im_num
 end
 meansRGB = uint8(meansRGB./im_num);
 imshow(meansRGB);
-imwrite(meansRGB,'mean_sRGB_ARW2TIF_TIF2PNG.png');
+imwrite(meansRGB,'meansRGB_ARW2TIF_TIF2PNG.png');
 clear sRGB meansRGB;
 
 
