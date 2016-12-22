854f65610c1aa26f7546d707d79fe9704a05a434
XuJun <nankaimathxujun@gmail.com>
2016-12-22 11:21:25 +0800

    ADD

--- DcrawPipeline_Mean.m
+++ DcrawPipeline_Mean.m
@@ -1,5 +1,5 @@
 clear;
-Original_image_dir = './20161221mean';
+Original_image_dir = '20161221mean/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
