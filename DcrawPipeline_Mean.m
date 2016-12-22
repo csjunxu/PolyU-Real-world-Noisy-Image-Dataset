765eb4458f780a1dcc48b6292a1ea77636c4949d
XuJun <nankaimathxujun@gmail.com>
2016-12-22 15:35:28 +0800

    add

--- DcrawPipeline_Mean.m
+++ DcrawPipeline_Mean.m
@@ -1,6 +1,6 @@
 clear;
 Original_image_dir = '20161221mean/';
-fpath = fullfile(Original_image_dir, 'meanRawGT*.tiff');
+fpath = fullfile(Original_image_dir, 'RawGT*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
 for i = 1:im_num
