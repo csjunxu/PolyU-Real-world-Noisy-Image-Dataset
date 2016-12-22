46aa93331c1d90069857e0990e926e55f23c3fec
XuJun <nankaimathxujun@gmail.com>
2016-12-22 17:04:55 +0800

    add

---
+++ systemcode.m
@@ -0,0 +1,4 @@
+system('del *TIFF2PNG.png');
+system('del *.png');
+system('dcraw -v -T C:\Users\csjunxu\Desktop\Projects\RID_Dataset\checkparameters\DSC01613.ARW');
+system('dcraw -v -w C:\Users\csjunxu\Desktop\Projects\RID_Dataset\checkparameters\DSC01613.ARW');
