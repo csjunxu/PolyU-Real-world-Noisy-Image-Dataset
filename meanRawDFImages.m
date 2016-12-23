b604e1b2c5b7f618a62c028ee145be493175365f
XuJun <nankaimathxujun@gmail.com>
2016-12-23 19:20:51 +0800

    add

--- meanRawDFImages.m
+++ meanRawDFImages.m
@@ -1,5 +1,5 @@
 %% mean of dark frames
-Original_image_dir = '20161221DF/';
+Original_image_dir = '20161222DF/';
 fpath = fullfile(Original_image_dir, '*.pgm');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
