5f8b46520a1439566c708cadc236e6d53beed83a
XuJun <nankaimathxujun@gmail.com>
2016-12-24 14:57:20 +0800

    add

--- Cal_PSNRSSIM_RAWsRGB.m
+++ Cal_PSNRSSIM_RAWsRGB.m
@@ -1,12 +1,12 @@
 clear;
 %% mean of raw images
-Original_image_dir = '20161223/';
+Original_image_dir = '20161222/';
 fpath = fullfile(Original_image_dir, '*.png');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-meanRAW = imread('20161223mean/meanRAW_ARW2TIF_TIF2PNG.png');
-RAWGT = imread('20161223mean/RawGT_ARW2TIF_TIF2PNG.png');
-meansRGB = imread('20161223mean/meansRGB_ARW2TIF_TIF2PNG.png');
+meanRAW = imread('20161222mean/meanRAW_ARW2TIF_TIF2PNG.png');
+RAWGT = imread('20161222mean/RawGT_ARW2TIF_TIF2PNG.png');
+meansRGB = imread('20161222mean/meansRGB_ARW2TIF_TIF2PNG.png');
 fprintf('The PSNR/SSIM of meanRAW and meansRGB are %2.4f/%2.4f. \n', csnr( meansRGB,meanRAW, 0, 0 ), cal_ssim( meansRGB, meanRAW, 0, 0 ));
 fprintf('The PSNR/SSIM of RAW_GT and meansRGB are %2.4f/%2.4f. \n', csnr( meansRGB,RAWGT, 0, 0 ), cal_ssim( meansRGB, RAWGT, 0, 0 ));
 PSNR_meanRAW = [];
@@ -37,7 +37,7 @@ mPSNR_RAWGT = mean(PSNR_RAWGT);
 mSSIM_RAWGT = mean(SSIM_RAWGT);
 mPSNR_meansRGB = mean(PSNR_meansRGB);
 mSSIM_meansRGB = mean(SSIM_meansRGB);
-savename = ['PSNRSSIM_meanRAW_RAWGT_meansRGB_20161223_ISO1600.mat'];
+savename = ['PSNRSSIM_meanRAW_RAWGT_meansRGB_20161222_ISO6400.mat'];
 save(savename, 'mPSNR_meanRAW', 'mSSIM_meanRAW', 'PSNR_meanRAW', 'SSIM_meanRAW',...
     'mPSNR_RAWGT', 'mSSIM_RAWGT', 'PSNR_RAWGT', 'SSIM_RAWGT',...
     'mPSNR_meansRGB','mSSIM_meansRGB','PSNR_meansRGB','SSIM_meansRGB');
