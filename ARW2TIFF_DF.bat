f7172f133e975ff9eb1cedc79082e29e7dc72f27
XuJun <nankaimathxujun@gmail.com>
2016-12-24 11:20:22 +0800

    add

--- ARW2TIFF_DF.bat
+++ ARW2TIFF_DF.bat
@@ -1,2 +1,2 @@
-for /r C:\Users\csjunxu\Desktop\Projects\RID_Dataset\20161223DF\ %%i in (*.ARW) do dcraw -D -4 -j -t 0 %%i
-for /r C:\Users\csjunxu\Desktop\Projects\RID_Dataset\20161223DF\ %%i in (*.ARW) do dcraw -4 -T -D -v %%i
+for /r C:\Users\csjunxu\Desktop\Projects\RID_Dataset\20161224DF\ %%i in (*.ARW) do dcraw -D -4 -j -t 0 %%i
+for /r C:\Users\csjunxu\Desktop\Projects\RID_Dataset\20161224DF\ %%i in (*.ARW) do dcraw -4 -T -D -v %%i
