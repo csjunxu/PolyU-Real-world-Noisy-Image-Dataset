4c6ada1b888aafe7e2a9b8012a21517c78cd3a8a
XuJun <nankaimathxujun@gmail.com>
2016-12-26 16:38:03 +0800

    add

--- meansRGBImages.m
+++ meansRGBImages.m
@@ -16,8 +16,23 @@ for i = 1:im_num
 end
 meansRGB = uint8(meansRGB./im_num);
 imshow(meansRGB);
-imwrite(meansRGB,'20161226mean_ISO3200_5000/meansRGB_ARW2TIF_TIF2PNG.png');
+imwrite(meansRGB,'20161226mean_ISO3200_5000/meansRGBAll_ARW2TIF_TIF2PNG.png');
 clear sRGB meansRGB;
+
+meansRGB = zeros(size(sRGB)); 
+for i = 1:min(500,im_num)
+    %% read the tiff image 
+    sRGB = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
+    S = regexp(im_dir(i).name, '\.', 'split');
+    rawname = S{1};
+    fprintf('Processing %s. \n', rawname);
+    meansRGB = meansRGB + sRGB;
+end
+meansRGB = uint8(meansRGB./im_num);
+imshow(meansRGB);
+imwrite(meansRGB,'20161226mean_ISO3200_5000/meansRGB500_ARW2TIF_TIF2PNG.png');
+clear sRGB meansRGB;
+
 
 
 
