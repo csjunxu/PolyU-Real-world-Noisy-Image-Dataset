c164077f7d3b3b86542ac45bea3356e542e37dda
XuJun <nankaimathxujun@gmail.com>
2016-12-26 13:45:57 +0800

    add

--- ARW2TIFF.bat
+++ ARW2TIFF.bat
@@ -1,1 +1,1 @@
-for /r C:\Users\csjunxu\Desktop\Projects\RID_Dataset\20161226_ISO3200_5000\ %%i in (*.ARW) do dcraw -4 -T -D -v %%i
+for /r C:\Users\csjunxu\Desktop\Projects\RID_Dataset\20161226\ %%i in (*.ARW) do dcraw -4 -T -D -v %%i
