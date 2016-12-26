26f2e2c9a451062cac8dc2859f1fcbf3f4c21a2c
XuJun <nankaimathxujun@gmail.com>
2016-12-26 23:03:18 +0800

    add

--- Cal_PSNRSSIM_RAWsRGB.m
+++ Cal_PSNRSSIM_RAWsRGB.m
@@ -21,26 +21,26 @@ for i = 1:im_num
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
-        PSNR_meansRGBAll = [PSNR_meansRGBAll csnr( IMin,meansRGBAll, 0, 0 )];
+    PSNR_meansRGBAll = [PSNR_meansRGBAll csnr( IMin,meansRGBAll, 0, 0 )];
     SSIM_meansRGBAll = [SSIM_meansRGBAll cal_ssim( IMin, meansRGBAll, 0, 0 )];
     PSNR_meanRAWAll = [PSNR_meanRAWAll csnr( IMin,meanRAWAll, 0, 0 )];
     SSIM_meanRAWAll = [SSIM_meanRAWAll cal_ssim( IMin, meanRAWAll, 0, 0 )];
     PSNR_RAWGTAll = [PSNR_RAWGTAll csnr( IMin,RAWGTAll, 0, 0 )];
     SSIM_RAWGTAll = [SSIM_RAWGTAll cal_ssim( IMin, RAWGTAll, 0, 0 )];
-     fprintf('The PSNR/SSIM of  meansRGB are %2.4f/%2.4f. \n', PSNR_meansRGBAll(end), SSIM_meansRGBAll(end));
+    fprintf('The PSNR/SSIM of  meansRGB are %2.4f/%2.4f. \n', PSNR_meansRGBAll(end), SSIM_meansRGBAll(end));
     fprintf('The PSNR/SSIM of meanRAW are %2.4f/%2.4f. \n', PSNR_meanRAWAll(end), SSIM_meanRAWAll(end));
     fprintf('The PSNR/SSIM of RAWGT are %2.4f/%2.4f. \n', PSNR_RAWGTAll(end), SSIM_RAWGTAll(end));
 end
-mPSNR_meansRGB = mean(PSNR_meansRGBAll);
-mSSIM_meansRGB = mean(SSIM_meansRGBAll);
-mPSNR_meanRAW = mean(PSNR_meanRAWAll);
-mSSIM_meanRAW = mean(SSIM_meanRAWAll);
-mPSNR_RAWGT = mean(PSNR_RAWGTAll);
-mSSIM_RAWGT = mean(SSIM_RAWGTAll);
+mPSNR_meansRGBAll = mean(PSNR_meansRGBAll);
+mSSIM_meansRGBAll = mean(SSIM_meansRGBAll);
+mPSNR_meanRAWAll = mean(PSNR_meanRAWAll);
+mSSIM_meanRAWAll = mean(SSIM_meanRAWAll);
+mPSNR_RAWGTAll = mean(PSNR_RAWGTAll);
+mSSIM_RAWGTAll = mean(SSIM_RAWGTAll);
 savename = ['PSNRSSIM_meansRGBAll_meanRAWAll_RAWGTAll_20161226_ISO3200.mat'];
-save(savename, 'mPSNR_meanRAW', 'mSSIM_meanRAW', 'PSNR_meanRAW', 'SSIM_meanRAW',...
-    'mPSNR_RAWGT', 'mSSIM_RAWGT', 'PSNR_RAWGT', 'SSIM_RAWGT',...
-    'mPSNR_meansRGB','mSSIM_meansRGB','PSNR_meansRGB','SSIM_meansRGB');
+save(savename, 'mPSNR_meansRGBAll','mSSIM_meansRGBAll','PSNR_meansRGBAll','SSIM_meansRGBAll', ...
+    'mPSNR_meanRAWAll', 'mSSIM_meanRAWAll', 'PSNR_meanRAWAll', 'SSIM_meanRAWAll',...
+    'mPSNR_RAWGTAll', 'mSSIM_RAWGTAll', 'PSNR_RAWGTAll', 'SSIM_RAWGTAll');
 
 
 
