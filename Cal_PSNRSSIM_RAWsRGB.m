276fa126ec2d1d5866bb642322fd083286fa3772
XuJun <nankaimathxujun@gmail.com>
2016-12-26 23:02:07 +0800

    add

--- Cal_PSNRSSIM_RAWsRGB.m
+++ Cal_PSNRSSIM_RAWsRGB.m
@@ -1,43 +1,43 @@
 clear;
 %% mean of raw images
-Original_image_dir = '20161225/';
+Original_image_dir = '20161226_ISO3200_5000/';
 fpath = fullfile(Original_image_dir, '*.png');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-meanRAW = imread('20161225mean/meanRAW_ARW2TIF_TIF2PNG.png');
-RAWGT = imread('20161225mean/RawGT_ARW2TIF_TIF2PNG.png');
-meansRGB = imread('20161225mean/meansRGB_ARW2TIF_TIF2PNG.png');
-fprintf('The PSNR/SSIM of meanRAW/meansRGB are %2.4f/%2.4f. \n', csnr( meansRGB,meanRAW, 0, 0 ), cal_ssim( meansRGB, meanRAW, 0, 0 ));
-fprintf('The PSNR/SSIM of RAW_GT/meansRGB are %2.4f/%2.4f. \n', csnr( meansRGB,RAWGT, 0, 0 ), cal_ssim( meansRGB, RAWGT, 0, 0 ));
-PSNR_meanRAW = [];
-SSIM_meanRAW = [];
-PSNR_RAWGT = [];
-SSIM_RAWGT = [];
-PSNR_meansRGB = [];
-SSIM_meansRGB = [];
+meanRAWAll = imread('20161226mean_ISO3200_5000/meanRAWAll_ARW2TIF_TIF2PNG.png');
+RAWGTAll = imread('20161226mean_ISO3200_5000/RawGTAll_ARW2TIF_TIF2PNG.png');
+meansRGBAll = imread('20161226mean_ISO3200_5000/meansRGB_ARW2TIF_TIF2PNG.png');
+fprintf('The PSNR/SSIM of meanRAW/meansRGB are %2.4f/%2.4f. \n', csnr( meansRGBAll,meanRAWAll, 0, 0 ), cal_ssim( meansRGBAll, meanRAWAll, 0, 0 ));
+fprintf('The PSNR/SSIM of RAW_GT/meansRGB are %2.4f/%2.4f. \n', csnr( meansRGBAll,RAWGTAll, 0, 0 ), cal_ssim( meansRGBAll, RAWGTAll, 0, 0 ));
+PSNR_meansRGBAll = [];
+SSIM_meansRGBAll = [];
+PSNR_meanRAWAll = [];
+SSIM_meanRAWAll = [];
+PSNR_RAWGTAll = [];
+SSIM_RAWGTAll = [];
 for i = 1:im_num
     %% read the tiff image
     IMin = imread(fullfile(Original_image_dir, im_dir(i).name));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
-    PSNR_meanRAW = [PSNR_meanRAW csnr( IMin,meanRAW, 0, 0 )];
-    SSIM_meanRAW = [SSIM_meanRAW cal_ssim( IMin, meanRAW, 0, 0 )];
-    PSNR_RAWGT = [PSNR_RAWGT csnr( IMin,RAWGT, 0, 0 )];
-    SSIM_RAWGT = [SSIM_RAWGT cal_ssim( IMin, RAWGT, 0, 0 )];
-    PSNR_meansRGB = [PSNR_meansRGB csnr( IMin,meansRGB, 0, 0 )];
-    SSIM_meansRGB = [SSIM_meansRGB cal_ssim( IMin, meansRGB, 0, 0 )];
-    fprintf('The PSNR/SSIM of meanRAW are %2.4f/%2.4f. \n', PSNR_meanRAW(end), SSIM_meanRAW(end));
-    fprintf('The PSNR/SSIM of RAWGT are %2.4f/%2.4f. \n', PSNR_RAWGT(end), SSIM_RAWGT(end));
-    fprintf('The PSNR/SSIM of  meansRGB are %2.4f/%2.4f. \n', PSNR_meansRGB(end), SSIM_meansRGB(end));
+        PSNR_meansRGBAll = [PSNR_meansRGBAll csnr( IMin,meansRGBAll, 0, 0 )];
+    SSIM_meansRGBAll = [SSIM_meansRGBAll cal_ssim( IMin, meansRGBAll, 0, 0 )];
+    PSNR_meanRAWAll = [PSNR_meanRAWAll csnr( IMin,meanRAWAll, 0, 0 )];
+    SSIM_meanRAWAll = [SSIM_meanRAWAll cal_ssim( IMin, meanRAWAll, 0, 0 )];
+    PSNR_RAWGTAll = [PSNR_RAWGTAll csnr( IMin,RAWGTAll, 0, 0 )];
+    SSIM_RAWGTAll = [SSIM_RAWGTAll cal_ssim( IMin, RAWGTAll, 0, 0 )];
+     fprintf('The PSNR/SSIM of  meansRGB are %2.4f/%2.4f. \n', PSNR_meansRGBAll(end), SSIM_meansRGBAll(end));
+    fprintf('The PSNR/SSIM of meanRAW are %2.4f/%2.4f. \n', PSNR_meanRAWAll(end), SSIM_meanRAWAll(end));
+    fprintf('The PSNR/SSIM of RAWGT are %2.4f/%2.4f. \n', PSNR_RAWGTAll(end), SSIM_RAWGTAll(end));
 end
-mPSNR_meanRAW = mean(PSNR_meanRAW);
-mSSIM_meanRAW = mean(SSIM_meanRAW);
-mPSNR_RAWGT = mean(PSNR_RAWGT);
-mSSIM_RAWGT = mean(SSIM_RAWGT);
-mPSNR_meansRGB = mean(PSNR_meansRGB);
-mSSIM_meansRGB = mean(SSIM_meansRGB);
-savename = ['PSNRSSIM_meanRAW_RAWGT_meansRGB_20161225_ISO100.mat'];
+mPSNR_meansRGB = mean(PSNR_meansRGBAll);
+mSSIM_meansRGB = mean(SSIM_meansRGBAll);
+mPSNR_meanRAW = mean(PSNR_meanRAWAll);
+mSSIM_meanRAW = mean(SSIM_meanRAWAll);
+mPSNR_RAWGT = mean(PSNR_RAWGTAll);
+mSSIM_RAWGT = mean(SSIM_RAWGTAll);
+savename = ['PSNRSSIM_meansRGBAll_meanRAWAll_RAWGTAll_20161226_ISO3200.mat'];
 save(savename, 'mPSNR_meanRAW', 'mSSIM_meanRAW', 'PSNR_meanRAW', 'SSIM_meanRAW',...
     'mPSNR_RAWGT', 'mSSIM_RAWGT', 'PSNR_RAWGT', 'SSIM_RAWGT',...
     'mPSNR_meansRGB','mSSIM_meansRGB','PSNR_meansRGB','SSIM_meansRGB');
