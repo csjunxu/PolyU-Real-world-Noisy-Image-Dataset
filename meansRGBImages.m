b92ffcb7e8ea0f4bbdcf1ebf9e730afb7f9a4365
XuJun <nankaimathxujun@gmail.com>
2016-12-26 22:54:31 +0800

    add

--- meansRGBImages.m
+++ meansRGBImages.m
@@ -5,34 +5,26 @@ fpath = fullfile(Original_image_dir, '*.png');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
 sRGB = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
-meansRGB = zeros(size(sRGB)); 
+meansRGBAll = zeros(size(sRGB));
+meansRGB500 = zeros(size(sRGB));
 for i = 1:im_num
-    %% read the tiff image 
+    %% read the tiff image
     sRGB = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
     fprintf('Processing %s. \n', rawname);
-    meansRGB = meansRGB + sRGB;
+    meansRGBAll = meansRGBAll + sRGB;
+    meansRGB500 = meansRGB500 + sRGB;
+    if i == min(500,im_num)
+        meansRGB500 = uint8(meansRGB500./min(500,im_num));
+        imshow(meansRGB500);
+        imwrite(meansRGB500,'20161226mean_ISO3200_5000/meansRGB500_ARW2TIF_TIF2PNG.png');
+        clear meansRGB500;
+    end
 end
-meansRGB = uint8(meansRGB./im_num);
-imshow(meansRGB);
-imwrite(meansRGB,'20161226mean_ISO3200_5000/meansRGBAll_ARW2TIF_TIF2PNG.png');
-clear sRGB meansRGB;
-
-meansRGB = zeros(size(sRGB)); 
-for i = 1:min(500,im_num)
-    %% read the tiff image 
-    sRGB = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
-    S = regexp(im_dir(i).name, '\.', 'split');
-    rawname = S{1};
-    fprintf('Processing %s. \n', rawname);
-    meansRGB = meansRGB + sRGB;
-end
-meansRGB = uint8(meansRGB./min(500,im_num));
-imshow(meansRGB);
-imwrite(meansRGB,'20161226mean_ISO3200_5000/meansRGB500_ARW2TIF_TIF2PNG.png');
-clear sRGB meansRGB;
-
-
+meansRGBAll = uint8(meansRGBAll./im_num);
+% imshow(meansRGBAll);
+imwrite(meansRGBAll,'20161226mean_ISO3200_5000/meansRGBAll_ARW2TIF_TIF2PNG.png');
+clear sRGB meansRGBAll;
 
 
