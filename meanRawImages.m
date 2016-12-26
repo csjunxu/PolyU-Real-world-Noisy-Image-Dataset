f3faf9f995f092e4f24237a9fa5d078d34c3f880
XuJun <nankaimathxujun@gmail.com>
2016-12-26 22:58:00 +0800

    add

--- meanRawImages.m
+++ meanRawImages.m
@@ -4,41 +4,25 @@ Original_image_dir = '20161226_ISO3200_5000/';
 fpath = fullfile(Original_image_dir, '*.tiff');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
-raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
-meanRaw = zeros(size(raw)); 
+Raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
+meanRawAll = zeros(size(Raw));
+meanRaw500 = zeros(size(Raw));
 for i = 1:im_num
     %% read the tiff image
-    raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
+    Raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
     S = regexp(im_dir(i).name, '\.', 'split');
     rawname = S{1};
-    fprintf('Processing %s. \n', rawname);
-    meanRaw = meanRaw + raw;
+%     fprintf('Processing %s. \n', rawname);
+    meanRawAll = meanRawAll + Raw;
+    meanRaw500 = meanRaw500 + Raw;
+    if i == min(500,im_num)
+        meanRaw500 = uint16(meanRaw500./min(500,im_num));
+        imshow(meanRaw500);
+        imwrite(meanRaw500,'20161226mean_ISO3200_5000/meanRaw500_ARW2TIF.tiff');
+        clear meanRaw500;
+    end
 end
-meanRaw = uint16(meanRaw./im_num);
-imshow(meanRaw);
-imwrite(meanRaw,['20161226mean_ISO3200_5000/meanRAWAll_ARW2TIF.tiff']);
-clear raw meanRaw;
-
-clear;
-%% mean of raw images
-Original_image_dir = '20161226_ISO3200_5000/';
-fpath = fullfile(Original_image_dir, '*.tiff');
-im_dir  = dir(fpath);
-im_num = length(im_dir);
-raw = double(imread(fullfile(Original_image_dir, im_dir(1).name)));
-meanRaw = zeros(size(raw)); 
-for i = 1:min(500,im_num)
-    %% read the tiff image
-    raw = double(imread(fullfile(Original_image_dir, im_dir(i).name)));
-    S = regexp(im_dir(i).name, '\.', 'split');
-    rawname = S{1};
-    fprintf('Processing %s. \n', rawname);
-    meanRaw = meanRaw + raw;
-end
-meanRaw = uint16(meanRaw./min(500,im_num));
-imshow(meanRaw);
-imwrite(meanRaw,['20161226mean_ISO3200_5000/meanRAW500_ARW2TIF.tiff']);
-clear raw meanRaw;
-
-
-
+meanRawAll = uint16(meanRawAll./im_num);
+% imshow(meansRGBAll);
+imwrite(meanRawAll,'20161226mean_ISO3200_5000/meanRawAll_ARW2TIF.tiff');
+clear Raw meanRawAll;
