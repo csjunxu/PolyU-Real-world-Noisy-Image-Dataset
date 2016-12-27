f6f57a639dedac0c63d9ace6d50bf2f375099b8f
XuJun <nankaimathxujun@gmail.com>
2016-12-27 08:12:37 +0800

    add

--- Cal_PSNRSSIM_RAWsRGB.m
+++ Cal_PSNRSSIM_RAWsRGB.m
@@ -31,25 +31,31 @@ for i = 1:im_num
     SSIM_meansRGBAll = [SSIM_meansRGBAll cal_ssim( IMin, meansRGBAll, 0, 0 )];
     PSNR_meanRAWAll = [PSNR_meanRAWAll csnr( IMin,meanRAWAll, 0, 0 )];
     SSIM_meanRAWAll = [SSIM_meanRAWAll cal_ssim( IMin, meanRAWAll, 0, 0 )];
-%     PSNR_RAWGTAll = [PSNR_RAWGTAll csnr( IMin,RAWGTAll, 0, 0 )];
-%     SSIM_RAWGTAll = [SSIM_RAWGTAll cal_ssim( IMin, RAWGTAll, 0, 0 )];
+    %     PSNR_RAWGTAll = [PSNR_RAWGTAll csnr( IMin,RAWGTAll, 0, 0 )];
+    %     SSIM_RAWGTAll = [SSIM_RAWGTAll cal_ssim( IMin, RAWGTAll, 0, 0 )];
     if i <= min(500,im_num)
-            PSNR_meansRGBAll = [PSNR_meansRGBAll csnr( IMin,meansRGBAll, 0, 0 )];
-    SSIM_meansRGBAll = [SSIM_meansRGBAll cal_ssim( IMin, meansRGBAll, 0, 0 )];
-    PSNR_meanRAWAll = [PSNR_meanRAWAll csnr( IMin,meanRAWAll, 0, 0 )];
-    SSIM_meanRAWAll = [SSIM_meanRAWAll cal_ssim( IMin, meanRAWAll, 0, 0 )];
-%     PSNR_RAWGTAll = [PSNR_RAWGTAll csnr( IMin,RAWGTAll, 0, 0 )];
-%     SSIM_RAWGTAll = [SSIM_RAWGTAll cal_ssim( IMin, RAWGTAll, 0, 0 )];
+        PSNR_meansRGB500 = PSNR_meansRGBAll;
+        SSIM_meansRGB500 = SSIM_meansRGBAll;
+        PSNR_meanRAW500 = PSNR_meanRAWAll;
+        SSIM_meanRAW500 = SSIM_meanRAWAll;
+        % PSNR_RAWGT500 = PSNR_RAWGTAll;
+        % SSIM_RAWGT500 = SSIM_RAWGTAll;
     end
     if i == min(500,im_num)
-        meanRaw500 = uint16(meanRaw500./min(500,im_num));
-%         imshow(meanRaw500);
-        imwrite(meanRaw500,'20161226mean_ISO3200_5000/meanRaw500_ARW2TIF.tiff');
-        clear meanRaw500;
+        mPSNR_meansRGB500 = mean(PSNR_meansRGB500);
+        mSSIM_meansRGB500 = mean(SSIM_meansRGB500);
+        mPSNR_meanRAW500 = mean(PSNR_meanRAW500);
+        mSSIM_meanRAW500 = mean(SSIM_meanRAW500);
+        % mPSNR_RAWGT500 = mean(PSNR_RAWGT500);
+        % mSSIM_RAWGT500 = mean(SSIM_RAWGT500);
+        savename = ['PSNRSSIM_meansRGB500_meanRAW500_RAWGT500_20161226_ISO3200.mat'];
+        save(savename, 'mPSNR_meansRGB500','mSSIM_meansRGB500','PSNR_meansRGB500','SSIM_meansRGB500', ...
+            'mPSNR_meanRAW500', 'mSSIM_meanRAW500', 'PSNR_meanRAW500', 'SSIM_meanRAW500');%,...
+        %     'mPSNR_RAWGT500', 'mSSIM_RAWGT500', 'PSNR_RAWGT500', 'SSIM_RAWGT500');
     end
     fprintf('The PSNR/SSIM of  meansRGB are %2.4f/%2.4f. \n', PSNR_meansRGBAll(end), SSIM_meansRGBAll(end));
     fprintf('The PSNR/SSIM of meanRAW are %2.4f/%2.4f. \n', PSNR_meanRAWAll(end), SSIM_meanRAWAll(end));
-%     fprintf('The PSNR/SSIM of RAWGT are %2.4f/%2.4f. \n', PSNR_RAWGTAll(end), SSIM_RAWGTAll(end));
+    %     fprintf('The PSNR/SSIM of RAWGT are %2.4f/%2.4f. \n', PSNR_RAWGTAll(end), SSIM_RAWGTAll(end));
 end
 mPSNR_meansRGBAll = mean(PSNR_meansRGBAll);
 mSSIM_meansRGBAll = mean(SSIM_meansRGBAll);
