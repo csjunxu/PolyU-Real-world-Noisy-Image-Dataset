1a2b51f0cdaf22719c5dccf0bd83bc9534b596f1
XuJun <nankaimathxujun@gmail.com>
2017-01-08 09:47:10 +0800

    a

--- meansRGB.m
+++ meansRGB.m
@@ -1,7 +1,7 @@
 clear;
 %% mean of raw images
-Original_image_dir = '20161230/';
-fpath = fullfile(Original_image_dir, '*.png');
+Original_image_dir = '20170108/';
+fpath = fullfile(Original_image_dir, '*.JPG');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
 D = regexp(Original_image_dir, '/', 'split');
@@ -15,16 +15,16 @@ for i = 1:im_num
     rawname = S{1};
 %     fprintf('Processing %s. \n', rawname);
     meansRGBAll = meansRGBAll + sRGB;
-    if i == min(500,im_num)
-        meansRGB500 = uint8(meansRGBAll./min(500,im_num));
+    if mod(i, 500) == 0
+        meansRGB500 = uint8(meansRGBAll./i);
         %         imshow(meansRGB500);
-        imwrite(meansRGB500,[D{1} 'mean/meansRGB500_ARW2TIF_TIF2PNG.png']);
+        imwrite(meansRGB500,[D{1} 'mean/meansRGB' num2str(i) '.JPG']);
         clear meansRGB500;
     end
 end
-meansRGBAll = uint8(meansRGBAll./im_num);
-% imshow(meansRGBAll);
-imwrite(meansRGBAll,[D{1} 'mean/meansRGBAll_ARW2TIF_TIF2PNG.png']);
-clear sRGB meansRGBAll;
+% meansRGBAll = uint8(meansRGBAll./im_num);
+% % imshow(meansRGBAll);
+% imwrite(meansRGBAll,[D{1} 'mean/meansRGBAll_ARW2TIF_TIF2PNG.JPG']);
+% clear sRGB meansRGBAll;
 
 
