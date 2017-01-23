bd9f30422168dc295f1b873a5db5877f453dbf02
XuJun <nankaimathxujun@gmail.com>
2017-01-23 22:28:37 +0800

    add

---
+++ runallmethods.m
@@ -0,0 +1,6 @@
+run 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image&Video Denoising\BM3D TIP2007\code\DemoCBM3D.m';
+run 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image&Video Denoising\CSF CVPR2014\Demo_RID_GT.m';
+run 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image&Video Denoising\DnCNN-master\Demo_test_DnCNN_C.m';
+run 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image&Video Denoising\MLP CVPR2012\MLP\MLP_GT.m';
+run 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image&Video Denoising\TRD CVPR2015\TNRD\Demo_RID_GT.m';
+run 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image&Video Denoising\WNNM CVPR2014\WNNM\Demo_RealwithGT.m';
